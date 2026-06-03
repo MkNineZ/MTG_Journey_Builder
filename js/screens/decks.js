fd4ae6562c46908e50142171ec5e2f8d89a8217a
Mark Colomer <marcobos.tr@gmail.com>
2026-06-03 18:35:06 +0200

    padding adjustments

--- js/screens/decks.js
+++ js/screens/decks.js
@@ -410,8 +410,8 @@ function renderEditView() {
                             </select>
                         </div>
                         <div class="tabletop-header-stats">
-                            <div id="tabletop-mana-curve" class="mana-curve" style="height: 60px; margin: 0; padding: 0; min-height: 60px;"></div>
-                            <div id="tabletop-color-dist" style="display: flex; flex-direction: row; gap: 0.5rem; align-items: center; justify-content: flex-end; margin: 0; padding: 0; min-height: 60px;"></div>
+                            <div id="tabletop-mana-curve" class="mana-curve" style="height: 75px; margin: 0; padding: 0; min-height: 75px;"></div>
+                            <div id="tabletop-color-dist" style="display: flex; flex-direction: row; gap: 0.5rem; align-items: center; justify-content: flex-end; margin: 0; padding: 0; min-height: 75px;"></div>
                         </div>
                     </div>
                     <div class="tabletop-scroll-area">
